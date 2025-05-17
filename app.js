3d60037defa24433995d3d514d8d61ffb335069d
IagoSouzaSilva <iagos1366@gmail.com>
2025-05-17 16:09:16 -0300

    Update app.js
    troca de codigos

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
 const readlineSync = require('readline-sync');
 const { realizarSoma } = require('./soma.js');
+const { divisao } = require('./divisao.js');
 const { realizarSubtracao } = require('./subtracao.js');
+const { realizarMultiplicacao } = require('./multiplicacao.js');
 
-let a = readlineSync.questionFloat('Informe o primeiro valor: ');
-let b = readlineSync.questionFloat('Informe o segundo valor: ');
+console.log("Escolha a operação que você deseja:");
+console.log("1 - Para Somar");
+console.log("2 - Para Subtrair");
+console.log("3 - Para Multiplicar");
+console.log("4 - Para Dividir");
 
-let resultado = realizarSoma(a, b);
+let opcao = readlineSync.questionInt("Digite o número da operação: ");
 
-let resultadoSubtracao = realizarSubtracao(a, b);
+let a = readlineSync.questionFloat("Informe o primeiro valor: ");
+let b = readlineSync.questionFloat("Informe o segundo valor: ");
 
-console.log(`Resultado da soma: ${resultado}`);
+let resultado;
+
+switch (opcao) {
+    case 1:
+        resultado = realizarSoma(a, b);
+        break;
+    case 2:
+        resultado = realizarSubtracao(a, b);
+        break;
+    case 3:
+        resultado = multiplicar(a, b);
+        break;
+    case 4:
+        resultado = divisao(a, b);
+        if (resultado !== null) {
+        }
+        break;
+    default:
+        console.log("Opção inválida. Por favor, escolha uma operação válida.");
+}
