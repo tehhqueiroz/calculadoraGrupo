42bf282e990d9e94f6d523ad9cea80b6e0695840
unknown <karolina.meira@hotmail.com>
2025-05-17 15:48:46 -0300

    feature/subtracao

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const readlineSync = require('readline-sync');
 const { realizarSoma } = require('./soma.js');
+const { realizarSubtracao } = require('./subtracao.js');
 
 let a = readlineSync.questionFloat('Informe o primeiro valor: ');
 let b = readlineSync.questionFloat('Informe o segundo valor: ');
 
 let resultado = realizarSoma(a, b);
+
+let resultadoSubtracao = realizarSubtracao(a, b);
 
 console.log(`Resultado da soma: ${resultado}`);
