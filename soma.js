cbc6ca01339b93f037ba635058498b86cdd8fd1f
unknown <tehqueiroz@hotmail.com>
2025-05-17 14:54:39 -0300

    Feature / Soma

--- soma.js
+++ soma.js
@@ -1,1 +1,19 @@
-soma = a + b;
+class Soma {
+    constructor(a, b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    somar() {
+        return this.a + this.b;
+    }
+}
+
+function realizarSoma(a, b) {
+    const calc = new Soma(a, b);
+    const somando = calc.somar();
+    console.log(`A soma = ${somando} \n`);
+    return somando; 
+}
+
+module.exports = { Soma, realizarSoma };
