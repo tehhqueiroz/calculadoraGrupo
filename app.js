87123c6a737f2c3d1244e5fc8f499517a32e4b90
unknown <tehqueiroz@hotmail.com>
2025-05-17 15:09:27 -0300

    Reduzido o codigo e corrigido

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const { realizarSoma } = require('./soma.js');
 let a = readlineSync.questionFloat('Informe o primeiro valor: ');
 let b = readlineSync.questionFloat('Informe o segundo valor: ');
 
-
 let resultado = realizarSoma(a, b);
-
 
 console.log(`Resultado da soma: ${resultado}`);
--- soma.js
+++ soma.js
@@ -1,19 +1,7 @@
-class Soma {
-    constructor(a, b) {
-        this.a = a;
-        this.b = b;
-    }
-
-    somar() {
-        return this.a + this.b;
-    }
-}
-
 function realizarSoma(a, b) {
-    const calc = new Soma(a, b);
-    const somando = calc.somar();
+    const somando = a + b;
     console.log(`A soma = ${somando} \n`);
     return somando; 
 }
 
-module.exports = { Soma, realizarSoma };
+module.exports = { realizarSoma };
